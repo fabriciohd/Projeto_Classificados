3ed38e5a5f0905aceb01b459651a963def92701f
Fabricio Cunha <fabricio_hd@hotmail.com>
2019-09-20 14:24:41 -0300

    criada paginas de produtos, downgrade jquery por conta do bootstrap

--- pages/header.php
+++ pages/header.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
 
-    <script type="text/javascript" src="assets/js/jquery-3.4.1.mim.js"></script>
+    <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="assets/js/script.js"></script>
 </head>
